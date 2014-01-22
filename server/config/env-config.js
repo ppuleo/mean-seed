489dd31256e4753e0dd0fb99bc2bbeafd8a713aa
Phil Puleo <puleo.phil@gmail.com>
2014-01-21 18:23:46 -0800

    Heroku port sensitivity
    Heroku uses random high ports for staging/production, allow for env port to be set externally

--- server/config/env-config.js
+++ server/config/env-config.js
@@ -28,9 +28,11 @@ module.exports = {
         clientRoot: rootPath + '/client',
         app: {
             name: 'MEAN Seed - Development'
-        }
+        },
+        port: process.env.NODE_ENV.PORT || 9000
     },
     staging: {
+        name: 'staging',
         db: {
             url: '', // Your staging db connection string
             options: {} // Your staging db options
@@ -44,9 +46,11 @@ module.exports = {
         clientRoot: rootPath + '/client',
         app: {
             name: 'MEAN Seed - Staging'
-        }
+        },
+        port: process.env.NODE_ENV.PORT || 80
     },
     production: {
+        name: 'production',
         db: {
             url: '', // Your production db connection string
             options: {} // Your production db options
@@ -60,7 +64,8 @@ module.exports = {
         clientRoot: rootPath + '/client',
         app: {
             name: 'MEAN Seed - Production'
-        }
+        },
+        port: process.env.NODE_ENV.PORT || 80
     }
 
 };
